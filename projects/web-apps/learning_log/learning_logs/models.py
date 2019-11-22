d835daaeaedcc81d7f3ecca162e206d571588893
Sekhar <sekhartsr@gmail.com>
2019-11-22 14:36:04 +0800

    added web project changes

--- projects/web-apps/learning_log/learning_logs/models.py
+++ projects/web-apps/learning_log/learning_logs/models.py
@@ -5,8 +5,20 @@ from django.db import models
 class Topic(models.Model):
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
+    
 
-
-def __str__(self):
-    """Return string representation fo Model"""
-    return self.text
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey('Topic', on_delete=models.PROTECT)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+            """Return a string representation of the model."""
+            return self.text[:50] + "..."
